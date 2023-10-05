ed1165478cf3f3724f84dde6d6d9ba315a9c3c3f
Justin Angelo Perez <justin@redhq.com.au>
2023-10-05 08:21:10 +0400

    Fix style format

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -7,28 +7,33 @@ interface Props {
   opacity?: number;
 }
 
-const HeroSection = ({ title, description, backgroundImage, opacity = 0.4 }: Props) => {
-
-  const heroSectionStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    position: 'relative',
-  };
-
-  const overlayStyle = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(255, 255, 255, 0.20)',
-    opacity: opacity, 
-  };
-
+const HeroSection = ({
+  title,
+  description,
+  backgroundImage,
+  opacity = 0.4,
+}: Props) => {
   return (
-    <div className="relative px-6 py-40 lg:px-8" style={heroSectionStyle}>
-      <div style={overlayStyle}></div>
+    <div
+      className="relative px-6 py-40 lg:px-8"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(255, 255, 255, 0.20)",
+          opacity: opacity,
+        }}
+      ></div>
       <div className="mx-auto max-w-2xl text-center pb-10 pt-10 lg:pt-40">
         <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
           {title}
