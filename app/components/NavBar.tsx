8eb8f1789ae581e37dbbecc17bcdd8307e73837d
Justin Angelo Perez <justin@redhq.com.au>
2023-11-02 12:44:07 +0400

    Fix icons

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -23,22 +23,69 @@ const Navbar = () => {
 						<div className="flex space-x-6 text-white">
 							<a
 								href="mailto:info@atlastelecom.ae"
-								className="hover:text-blue-300"
+								className="flex items-center hover:text-blue-300"
 							>
-								<i className="lnr lnr-envelope align-middle"></i>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									className="w-6 h-6"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+									/>
+								</svg>
 								<span className="hidden md:inline ml-2">
 									info@atlastelecom.ae
 								</span>
 							</a>
-							<a href="tel:+97126127500" className="hover:text-blue-300">
-								<i className="icon icon-phone-call2 align-middle"></i>
+
+							<a
+								href="tel:+97126127500"
+								className="hover:text-blue-300 flex items-center"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									className="w-6 h-6"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+									/>
+								</svg>
 								<span className="hidden md:inline ml-2">
 									Tel: +971 2 612 7500
 								</span>
 							</a>
-							<span className="hidden lg:inline">
-								<i className="fas fa-fax align-middle"></i>
-								<span className="ml-2">Fax: +971 2 612 7599</span>
+
+							<span className="flex items-center hover:text-blue-300">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									className="w-6 h-6"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+									/>
+								</svg>
+
+								<span className="hidden md:inline ml-2">
+									Fax: +971 2 612 7599
+								</span>
 							</span>
 						</div>
 
@@ -46,7 +93,6 @@ const Navbar = () => {
 							<a
 								href="https://www.facebook.com/AtlasGroupUAE"
 								target="_blank"
-								rel="noopener noreferrer"
 								className="hover:text-blue-300"
 							>
 								<i className="fab fa-facebook-f"></i>
@@ -54,7 +100,6 @@ const Navbar = () => {
 							<a
 								href="https://www.instagram.com/atlasgroupuae/"
 								target="_blank"
-								rel="noopener noreferrer"
 								className="hover:text-blue-300"
 							>
 								<i className="fab fa-instagram"></i>
@@ -62,7 +107,6 @@ const Navbar = () => {
 							<a
 								href="https://www.youtube.com/channel/UCbvh4DssPmH1ia5OCSlxEjg"
 								target="_blank"
-								rel="noopener noreferrer"
 								className="hover:text-blue-300"
 							>
 								<i className="fab fa-youtube"></i>
